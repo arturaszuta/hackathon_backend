938be4303ef9374d6faee9d471bc3cef78a62fe5
Artie <arturaszuta@gmail.com>
2020-10-04 17:13:42 -0400

    Add route for sweden odiac data.

--- src/api/finalData.js
+++ src/api/finalData.js
@@ -47,6 +47,16 @@ router.get('/emissions', (req, res) => {
   }
 });
 
+router.get('/swedenodiac', (req, res) => {
+  pool.query('SELECT odiac20181k as odiac, latitude, longitude from sweden', (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      res.status(200).json(results.rows)
+    }
+  })
+});
+
 router.get('/', (req, res) => {
   pool.query('SELECT * FROM finaldata', (error, results) => {
     if (error) {
